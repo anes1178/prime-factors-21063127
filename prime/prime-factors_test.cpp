14041bae67c78d7577ca0aca746d52120edbf1d3
anes <as11004@naver.com>
2025-05-13 13:46:05 +0900

    tdd commit2

--- prime/prime-factors_test.cpp
+++ prime/prime-factors_test.cpp
@@ -3,9 +3,14 @@
 
 using namespace testing;
 
-TEST(a, b) {
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
+TEST(PrimeFactors, Of1) {
 	PrimeFactor p;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, p.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor p;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, p.of(2));
 }
